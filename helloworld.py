0386f648b760825b7b5a4e1cb921ed57cd1347fa
Samih Abdeljabbar <49135655+samih-adam@users.noreply.github.com>
2020-05-08 20:57:10 -0500

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,7 @@
-
+print("Hello World")
+name = "Samih"
+age = "22"
+occupation = "Python Developer"
+print("my name is " + name)
+print("my age is " + age)
+print(", and the job that I would love to be in is " + occupation)
